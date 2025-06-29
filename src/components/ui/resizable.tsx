0f4c6e6a7b5038c8c7d766d9c74e8ac4838fb987
Kent Garcia <kentgarcia.dgl@gmail.com>
2025-06-29 22:52:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -3,10 +3,12 @@
 import * as React from "react"
 import { GripVertical } from "lucide-react"
 import {
-  ResizablePanelGroup as ResizablePanelGroupPrimitive,
-  type ResizablePanelGroupProps,
-  type ResizablePanelProps,
-  type ResizableHandleProps,
+  PanelGroup,
+  Panel,
+  PanelResizeHandle,
+  type PanelGroupProps,
+  type PanelProps,
+  type PanelResizeHandleProps,
 } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
@@ -14,8 +16,8 @@ import { cn } from "@/lib/utils"
 const ResizablePanelGroup = ({
   className,
   ...props
-}: ResizablePanelGroupProps) => (
-  <ResizablePanelGroupPrimitive
+}: PanelGroupProps) => (
+  <PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
@@ -24,14 +26,14 @@ const ResizablePanelGroup = ({
   />
 )
 
-const ResizablePanel = ResizablePanelGroupPrimitive.Panel
+const ResizablePanel = Panel
 
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: ResizableHandleProps & { withHandle?: boolean }) => (
-  <ResizablePanelGroupPrimitive.Handle
+}: PanelResizeHandleProps & { withHandle?: boolean }) => (
+  <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -43,7 +45,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizablePanelGroupPrimitive.Handle>
+  </PanelResizeHandle>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
